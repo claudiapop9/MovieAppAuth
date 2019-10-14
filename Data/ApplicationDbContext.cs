7f86bf0dce591944e89dfc6a71f499d03e6a7e6f
Claudia <popclaudia97@gmail.com>
2019-10-14 16:53:44 +0300

    second commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MovieAppAuth.Models;
+
+namespace MovieAppAuth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
